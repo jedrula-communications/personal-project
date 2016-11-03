491e41a0c41f1fe5ffb062ffb4838bd383c66bad
jedrula <andrzej.swaton@gmail.com>
2016-11-03 19:50:53 +0100

    delete button and listing for posts

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,9 +1,18 @@
 import Ember from 'ember';
 
+const { computed } = Ember;
+
 export default Ember.Route.extend({
+  model() {
+    return this.get('store').findAll('post');
+  },
+
   actions: {
     persist(form) {
       return this.get('store').createRecord('post', form).save();
+    },
+    remove(post) {
+      return post.destroyRecord();
     }
   }
 });
