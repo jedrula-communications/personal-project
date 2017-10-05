7082ca3b61e0aadca3df2b03aa33c8acb9e5e73a
jedrula <andrzej.swaton@gmail.com>
2017-10-05 14:01:41 +0200

    fix skills page bug and add more skills

--- app/controllers/skills.js
+++ app/controllers/skills.js
@@ -1,5 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  skills: ['js', 'scss', 'nodejs', 'heroku', 'ember'],
+  skills: [
+    'javascript',
+    'nodejs',
+    'heroku',
+    'ember',
+    'git',
+    'github',
+    'aws',
+    'scss',
+    'express',
+    'pug',
+    'mongodb',
+    'cloudinary',
+    'raspberrypi',
+    'jenkins',
+    'jquery',
+    'babel',
+    'knockoutjs',
+    'svg',
+    'socketio',
+  ],
 });
